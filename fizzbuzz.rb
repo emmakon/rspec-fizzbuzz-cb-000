dea0f0580cb5e32a78a4ddfd94e906c3d2f2209d
Emma Konrad <emma.konrad92@gmail.com>
2018-08-08 13:42:39 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,10 @@
 def fizzbuzz(int)
   if int % 3 == 0 
     return "Fizz"
-  elsif
+  elsif int % 5 == 0 
+    return "Buzz"
+  elsif int % 15 == 0 
+    return "FizzBuzz"
+  else
+    return nil 
 end
